73e7dd52acbc942a9c5f8df786a06d7fa6c6ddbe
MSTGr8 <64365070+MSTGr8@users.noreply.github.com>
2020-12-27 17:11:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,18 +30,18 @@ function show(){
     localStorage.setItem("notes" , JSON.stringify(storedObj));
   storedObj.forEach(function(element, index){
      html+=`<div class="bg-white rounded-lg">
-    <div class="w-96 border-t-8 border-pink-600 rounded-lg flex">
+    <div class="w-96 border-t-8 border-gray-900 rounded-lg flex">
       <div class="w-1/3 pt-6 flex justify-center">
         
       </div>
       <div class="w-full pt-9 pr-4">
-        <h3 class="font-bold text-green-700">`+(index+1)+`</h3>
-        <p class="py-4 text-sm text-purple-400">`+element+`</p>
+        <h3 class="font-bold text-gray-500">`+(index+1)+`</h3>
+        <p class="py-4 text-sm text-gray-600">`+element+`</p>
       </div>
     </div>
 
     <div class="p-4 flex space-x-4">
-     <button id=`+index+` onclick="deleteme(this.id)" class="w-1/2 px-4 py-3 text-center text-gray-600 bg-gray-900 rounded-lg hover:bg-pink-700 hover:text-white font-bold text-sm">Delete Note</button>
+     <button id=`+index+` onclick="deleteme(this.id)" class="w-1/2 px-4 py-3 text-center bg-gray-900 rounded-lg hover:bg-pink-700 hover:text-white font-bold text-sm">Delete Note</button>
     </div>
   </div>`;
     });
